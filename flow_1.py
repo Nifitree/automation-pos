9d23302f5f02592b645e92ccd154ebe866bade40
Nifitree <nifitree28@gmail.com>
2026-03-05 13:55:36 +0700

    5

--- flow_1.py
+++ flow_1.py
@@ -29,7 +29,7 @@ def connect_program():
 def send_parcel(main):
 
     # เข้าหน้า รับฝากสิ่งของ
-    main.child_window(title="รับฝากสิ่งของ", found_index=1).click()
+    main.child_window(title="รับฝากสิ่งของ", found_index=1).click_input()
     time.sleep(2)
 
     # อ่านบัตรประชาชน
